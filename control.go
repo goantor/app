17c37178c96d0413730f9bc6b8d93009e488557d
leaf <goants@163.com>
2023-12-23 20:16:50 +0800

    xxx

---
+++ control.go
@@ -0,0 +1,125 @@
+package app
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+/*
+	简单二次封装使用以下两个包
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+*/
+
+func NewControl(name string) IControl {
+	return &control{
+		command: &cobra.Command{
+			Use:   name,
+			Short: name,
+		},
+	}
+}
+
+func NewControlWithRoute(name string, routeFunc ControlRouteFunc) IControl {
+	return &control{
+		command: &cobra.Command{
+			Use:   name,
+			Short: name,
+		},
+		routeFunc: routeFunc,
+	}
+}
+
+type control struct {
+	command   *cobra.Command
+	routeFunc ControlRouteFunc
+}
+
+func (c *control) AddRouteFunc(routeFunc ControlRouteFunc) {
+	c.routeFunc = routeFunc
+}
+
+func (c *control) makeOption(opts ...string) (opt ControlOption) {
+	opt = ControlOption{}
+	length := len(opts)
+	if length > 0 {
+		opt.Short = opts[0]
+	}
+
+	if length > 1 {
+		opt.Long = opts[1]
+	}
+
+	if length >= 2 {
+		opt.Example = opts[2]
+	}
+
+	return
+}
+
+func (c *control) addControl(cmd *cobra.Command) *control {
+	c.command.AddCommand(cmd)
+	return &control{
+		command: cmd,
+	}
+}
+
+func (c *control) Group(name string, opts ...string) IGroup {
+	opt := c.makeOption(opts...)
+
+	cmd := &cobra.Command{
+		Use:     name,
+		Short:   opt.Short,
+		Long:    opt.Long,
+		Example: opt.Example,
+	}
+
+	return c.addControl(cmd)
+}
+
+func (c *control) Handler(name string, handler IHandler, opts ...string) {
+	var param *pflag.FlagSet
+	opt := c.makeOption(opts...)
+	cmd := &cobra.Command{
+		Use:     name,
+		Short:   opt.Short,
+		Long:    opt.Long,
+		Example: opt.Example,
+		Run: func(cmd *cobra.Command, args []string) {
+			handler.Handle(cmd, param, args)
+		},
+	}
+
+	param = cmd.PersistentFlags()
+	handler.Params(param)
+	c.addControl(cmd)
+}
+
+func (c *control) Execute() error {
+	c.routeFunc(c)
+	return c.command.Execute()
+}
+
+type ControlRouteFunc func(route IGroup)
+
+type ControlOption struct {
+	Long    string
+	Short   string
+	Example string
+}
+
+type IHandler interface {
+	Params(set *pflag.FlagSet)
+	Handle(cmd *cobra.Command, param *pflag.FlagSet, args []string)
+}
+
+type IGroup interface {
+	Group(name string, opts ...string) IGroup
+	Handler(name string, handler IHandler, opts ...string)
+}
+
+type IControl interface {
+	IGroup
+	AddRouteFunc(routeFunc ControlRouteFunc)
+	Execute() error
+}
--- go.mod
+++ go.mod
@@ -33,6 +33,7 @@ require (
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -51,6 +52,7 @@ require (
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.17.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
